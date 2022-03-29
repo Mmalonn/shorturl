16bab41bbfefc16505b53b5110294ac62e05497d
marcos torres <marcosezequiel_torres@hotmail.com>
2022-03-29 18:48:15 -0300

    pruebas de plantilla con handlebars

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const {create}=require("express-handlebars");
+const hbs=create({
+    extname:".hbs"
+});
+
+app.engine(".hbs",hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views","./views");
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+
+})
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+app.listen(5000, ()=>{
+    console.log("servidor andando 😊");
+})
