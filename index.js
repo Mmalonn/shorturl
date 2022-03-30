f3479b33b50387126de9403bc03a5de9d3d08101
marcos torres <marcosezequiel_torres@hotmail.com>
2022-03-30 14:28:06 -0300

    creado el navbar como componente y redirigido cada elemento a su destino

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.get("/",(req,res)=>{
     const urls=[
         {origin: "www.google.com/ruta1", shortURL:"ruta1"},
         {origin: "www.google.com/ruta2", shortURL:"ruta2"},
-        {origin: "www.google.com/ruta3", shortURL:"ruta3"}
+        {origin: "www.google.com/ruta3", shortURL:"ruta3"},
+        {origin: "www.google.com/ruta4", shortURL:"ruta4"}
     ]
     res.render("home",{urls:urls});
 
@@ -26,6 +27,14 @@ app.get("/",(req,res)=>{
 
 app.get("/login",(req,res)=>{
     res.render("login");
+
+})
+app.get("/logout",(req,res)=>{
+    res.render("logout");
+
+})
+app.get("/signin",(req,res)=>{
+    res.render("signin");
 
 })
 
