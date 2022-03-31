4d417b67a7ac034e193cb905210af7bf775d6e50
marcos torres <marcosezequiel_torres@hotmail.com>
2022-03-31 09:00:47 -0300

    agregada carpeta y archivo controllers para pasar funciones de peticion

---
+++ controllers/homeController.js
@@ -0,0 +1,13 @@
+const leerUrls=async(req,res)=>{
+    const urls=[
+        {origin: "www.google.com/ruta1", shortURL:"ruta1"},
+        {origin: "www.google.com/ruta2", shortURL:"ruta2"},
+        {origin: "www.google.com/ruta3", shortURL:"ruta3"},
+        {origin: "www.google.com/ruta4", shortURL:"ruta4"}
+    ]
+    res.render("home",{urls:urls});
+}
+
+module.exports={
+    leerUrls,
+}
--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,12 @@
 const express=require('express');
+const { leerUrls } = require('../controllers/homeController');
 const router=express.Router();
 
 
 
 
 
-router.get("/",(req,res)=>{
-    const urls=[
-        {origin: "www.google.com/ruta1", shortURL:"ruta1"},
-        {origin: "www.google.com/ruta2", shortURL:"ruta2"},
-        {origin: "www.google.com/ruta3", shortURL:"ruta3"},
-        {origin: "www.google.com/ruta4", shortURL:"ruta4"}
-    ]
-    res.render("home",{urls:urls});
-
-})
+router.get("/",leerUrls)
 
 
 module.exports = router;
