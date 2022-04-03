f48d1c5d49d7931ce807308216c48d1eb24b4587
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-03 11:42:13 -0300

    agregada carpeta para controladores del auth y establecidas ahi la logica y redireccion a login y register

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,11 @@
 const express=require('express');
+const { loginForm, registerForm } = require('../controllers/authController');
 const router=express.Router();
 
-router.get("/login",(req,res)=>{
-    res.render("login");
+router.get("/login",loginForm);
+router.get("/register",registerForm);
 
-})
-router.get("/logout",(req,res)=>{
-    res.render("logout");
 
-})
-router.get("/signin",(req,res)=>{
-    res.render("signin");
 
-})
 
 module.exports = router;
