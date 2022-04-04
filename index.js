29e2140bb9ec944fff81c1be50bd86298afec248
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-04 14:11:41 -0300

    session y flash instalados, requeridos y usados

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const session=require("express-session");
+const flash=require("connect-flash");
 const {create}=require("express-handlebars");
 const hbs=create({
     extname:".hbs",
@@ -19,6 +21,14 @@ app.use(express.urlencoded({extended:true}))
 app.use("/", require('./routes/home'));
 app.use("/auth", require('./routes/auth'));
 app.use(express.static(__dirname + "/public"));
+app.use(session({
+    secret:"SW",
+    resave:false,
+    saveUninitialized:false,
+    name:"SM"
+}));
+app.use(flash());
+
 
 
 const PORT = process.env.PORT || 5000;
--- middlewares/urlValida.js
+++ middlewares/urlValida.js
@@ -2,16 +2,19 @@ const { URL } = require("url");
 const urlValidar = (req, res, next) => {
     try{
         const {origenUrl}=req.body;
-        const urlFrontend=new URL(origenUrl);
-        if(urlFrontend.origin !=="null"){
-            return next();
-        }else {
-            throw new Error ("no valida");
+        const urlFrontEnd = new URL(origenUrl);
+        if(urlFrontEnd.origenUrl !== "null"){
+            if(
+                urlFrontEnd.protocol==="http:"||
+                urlFrontEnd.protocol==="https:"
+            ){
+                return next();
+            }
         }
+        throw new Error("no valida");
+
     }catch(error){
-        console.log(error);
         return res.send("url no valida");
     }
 }
-
 module.exports = urlValidar;
