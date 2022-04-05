9f53705c6b782ff7fbc36cb528f8ef0bafd496d2
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-05 14:27:57 -0300

    agregado middleware passport, requerido, usado y configurado serialize y deserialize

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const session = require('express-session');
 const flash = require("connect-flash");
+const passport = require("passport");
 const {create}=require("express-handlebars");
+const User = require("./models/User");
 const hbs=create({
     extname:".hbs",
     partialsDir:["views/components"]
@@ -11,17 +13,30 @@ const hbs=create({
 require("dotenv").config();
 require("./database/db");
 
-
-app.engine(".hbs",hbs.engine);
-app.set("view engine", ".hbs");
-app.set("views","./views");
 app.use(session({
     secret:"SW",
     resave:false,
     saveUninitialized:false,
     name:"SM",
 }));
-app.use(flash())
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done)=>{
+    done(null,{id:user._id,userName:user.userName});
+});
+passport.deserializeUser(async(user,done)=>{
+    const userDB= await User.findById(user.id);
+    return done(null,{id:userDB._id,userName:userDB.userName})
+});
+
+
+
+app.engine(".hbs",hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views","./views");
 app.use(express.urlencoded({extended:true}))
 app.use("/", require('./routes/home'));
 app.use("/auth", require('./routes/auth'));
