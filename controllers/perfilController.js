4f7e62d6841df556a82b7fff6a5d8f9300329169
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-08 16:56:23 -0300

    prueba

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -44,17 +44,25 @@ module.exports.editarPerfil=async(req,res)=>{
             const dirFile=path.join(
                 __dirname,`../public/img/perfiles/${req.user.id}.${extension}`
             );
-            console.log(dirFile);
-            console.log(file.filepath);
+      
+            const viejo= file.filepath;
 
-            // // fs.renameSync(file.filepath,dirFile);
 
-            // const image= await Jimp.read(dirFile);
-            // image.resize(200,200).quality(90).writeAsync(dirFile);
 
-            // const user = await User.findById(req.user.id);
-            // user.imagen=`${req.user.id}.${extension}`;
-            // await user.save();
+            fs.renameSync(viejo,dirFile);
+
+
+
+
+
+
+
+            const image= await Jimp.read(dirFile);
+            image.resize(200,200).quality(90).writeAsync(dirFile);
+
+            const user = await User.findById(req.user.id);
+            user.imagen=`${req.user.id}.${extension}`;
+            await user.save();
 
             req.flash("mensajes",[{msg:"Imagen subida"}]);
 
